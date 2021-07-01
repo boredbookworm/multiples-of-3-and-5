8f968a3df059e4be0210f2dd5488fba4c9d2c57b
ming443 <77284525+boredbookworm@users.noreply.github.com>
2021-07-01 01:31:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def solution(number):
+    mult = []
+    sum = 0
+    for i in range(1,number):
+        if i % 3 == 0 or i % 5 == 0:
+            mult.append(i)
+    for item in mult:
+        sum += item
+    return sum
+print(solution(16))
